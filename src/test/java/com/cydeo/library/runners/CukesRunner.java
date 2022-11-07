fed2ba99e4c112ff9cf7e2b2b74d98ff837cbe32
Alexey Bukin <alexbukin90@gmail.com>
2022-11-07 14:17:19 -0500

    Project updated

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html",
+        plugin = {"json:target/cucumber.json",
+                "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
